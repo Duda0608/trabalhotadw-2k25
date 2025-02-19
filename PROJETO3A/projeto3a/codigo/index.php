b39a4acbc177c07b08dd57437b2e473a260be0f4
ana <anaalmeidaaam@gmail.com>
2025-02-19 10:49:36 -0300

    index login

--- PROJETO3A/projeto3a/codigo/index.php
+++ PROJETO3A/projeto3a/codigo/index.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>login</h1>
+    <form action="">
+        nome:
+        <input type="text" name="nome"><br><br>
+
+        email:
+        <input type="text" name="email"><br><br>
+
+        senha:
+        <input type="text" name="senha"><br>
+
+        <input type="submit" value="cadastrar">
+    </form>
 </body>
 </html>
