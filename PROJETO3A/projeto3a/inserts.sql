58650dc891b4ecef67efa7bfa1a4390e064bda6a
Nalaura2002 <anaalmeidaaam@gmail.com>
2025-02-26 10:55:22 -0300

    insert provisorio

--- PROJETO3A/projeto3a/inserts.sql
+++ PROJETO3A/projeto3a/inserts.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_cliente (nome, cpf, endereco) VALUES ("Fulano", "111.222.333-44", "Rua 1");
-INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade)
-VALUES ("Coca-cola", "Alimento", 5.50, 11.00, 0, 200);
+INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade) VALUES ("Coca-cola", "Alimento", 5.50, 11.00, 0, 200);
+INSERT INTO tb_usuario (nome, email, senha) VALUES ("Fulano", "fulano@gmail", "000");
+INSERT INTO tb_venda (valor_total, data) VALUES ("100", "2000-04-14");
